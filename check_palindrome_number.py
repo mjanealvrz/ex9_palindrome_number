971bf10fa55f19415e6c18c795783d6b5e55ef53
MJ Gillado <mjgillado0@gmail.com>
2024-01-24 01:31:53 +0800

    Created pseudocode

--- check_palindrome_number.py
+++ check_palindrome_number.py
@@ -1,2 +1,11 @@
 # Write a program to check if the given number is a palindrome number.
 
+# Pseudocode
+
+# Ask user to input  numbers
+# Create funtion to check if the input numbers are palindrome
+    # In the function, create a if-else statement
+    # If input number is palindrome return True
+    # Else, False
+
+# Print the result
