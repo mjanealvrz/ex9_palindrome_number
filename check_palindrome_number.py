f76994cdd03b9ffbbeb910b6fdb651c2e74ce5ee
MJ Gillado <mjgillado0@gmail.com>
2024-01-24 01:33:15 +0800

    User input interface

--- check_palindrome_number.py
+++ check_palindrome_number.py
@@ -3,6 +3,8 @@
 # Pseudocode
 
 # Ask user to input  numbers
+user_input = int(input("Please enter numbers: "))
+
 # Create funtion to check if the input numbers are palindrome
     # In the function, create a if-else statement
     # If input number is palindrome return True
